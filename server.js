f49377fe7c4d0594db20d6ab0142a66abeca0125
Samantha Arellano <Sambollano54@gmail.com>
2021-05-20 00:07:41 -0500

    .

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-const mysql = require("mysql2");
 const express = require('express');
 const inputCheck = require('./utils/inputCheck');
-
+const db = require('./db/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: '',
-  database: 'election'
-});
 
 // Get all candidates and their party affiliation
 app.get('/api/candidates', (req, res) => {
